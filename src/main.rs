80cb044048c0e8f7934bff113edafc0ee23fb6fa
Andrew Riachi <ariachi@ku.edu>
2025-03-28 17:56:26 -0500

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,27 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 only.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use procfs::process::{self, Process, MMapPath::*, MMPermissions};
-use procfs::ProcResult;
-use procfs::ProcError::{PermissionDenied, NotFound};
 use clap::Parser;
+use env_logger::Builder;
+use log::{warn, LevelFilter};
+use procfs::process::{self, MMPermissions, MMapPath::*, Process};
+use procfs::ProcError::{NotFound, PermissionDenied};
+use procfs::ProcResult;
 use regex;
 use std::collections::{HashMap, HashSet};
 use std::hash::RandomState;
 use std::thread;
 use std::time::Duration;
-use log::{warn, LevelFilter};
-use env_logger::Builder;
 
 #[derive(Parser)]
 #[command(version, about = "Reports process stack, heap, text, and data memory usage.", long_about = None)]
@@ -34,7 +34,7 @@ struct Args {
     ///If --regex is given, include children of matched processes, even if they don't match.
     #[arg(short = 'c', long, requires = "regex")]
     match_children: bool,
-    
+
     ///Refresh interval in seconds
     #[arg(short, long, default_value_t = 1.0_f64)]
     interval: f64,
@@ -46,15 +46,35 @@ struct Args {
 
     ///Print warnings to stderr
     #[arg(short = 'w', long)]
-    show_warnings: bool
+    show_warnings: bool,
 }
 
-struct ProcNode { pid: i32, ppid: i32, cmdline: String, process: Process, children: Vec<usize> }
+struct ProcNode {
+    pid: i32,
+    ppid: i32,
+    cmdline: String,
+    process: Process,
+    children: Vec<usize>,
+}
 // I might want a new type that has all the same information as ProcNode, but also with smaps.
 // function delegation for trait impls has been proposed in rust-lang/rfcs/#3530.
 // property delegation as in Kotlin would be nice.
-struct ProcListing { pid: i32, ppid: i32, cmdline: String, memory_ext: MemoryExt }
-struct MemoryExt { stack_pss: u64, heap_pss: u64, bin_text_pss: u64, lib_text_pss: u64, bin_data_pss: u64, lib_data_pss: u64, anon_map_pss: u64, vdso_pss: u64 }
+struct ProcListing {
+    pid: i32,
+    ppid: i32,
+    cmdline: String,
+    memory_ext: MemoryExt,
+}
+struct MemoryExt {
+    stack_pss: u64,
+    heap_pss: u64,
+    bin_text_pss: u64,
+    lib_text_pss: u64,
+    bin_data_pss: u64,
+    lib_data_pss: u64,
+    anon_map_pss: u64,
+    vdso_pss: u64,
+}
 fn main() {
     // Design: incrementally gather the data we need from each process
     // get_processes: () -> [{pid, ppid, cmdline, Process}]
@@ -74,7 +94,9 @@ fn main() {
     // the same as the length of the input list. At least, I know of no way to do this in Rust.
     let args = Args::parse();
     if args.show_warnings {
-        Builder::from_default_env().filter_level(LevelFilter::Warn).init();
+        Builder::from_default_env()
+            .filter_level(LevelFilter::Warn)
+            .init();
     } else {
         env_logger::init();
     }
@@ -110,47 +132,67 @@ fn filter_errors<T>(result: ProcResult<T>, fail_on_noperm: bool) -> Option<ProcR
 /// checked against `regex`. If there's a match, it will be included in the list.
 /// If `match_children` is `true`, then all children of the matched processes will
 /// also be included, whether their cmdline matches or not.
-fn get_processes(regex: &Option<regex::Regex>, match_children: bool, fail_on_noperm: bool) -> ProcResult<Vec<ProcNode>> {
+fn get_processes(
+    regex: &Option<regex::Regex>,
+    match_children: bool,
+    fail_on_noperm: bool,
+) -> ProcResult<Vec<ProcNode>> {
     let all_processes = process::all_processes()?;
-    let mut proc_tree = all_processes.filter_map(|proc_result| {
-        let combined_proc_info_result = proc_result.and_then(|process| {
-            process.stat().and_then(|stat| {
-            let pid = stat.pid;
-            let ppid = stat.ppid;
-            process.cmdline().and_then(|c| {
-            let cmdline = c.into_iter().fold("".to_owned(), |acc, val| acc + " " + &val); // TODO: why is this a Vec?
-            Ok((pid, ppid, cmdline, process))
-            })})}); // This should probably be illegal
+    let mut proc_tree = all_processes
+        .filter_map(|proc_result| {
+            let combined_proc_info_result = proc_result.and_then(|process| {
+                process.stat().and_then(|stat| {
+                    let pid = stat.pid;
+                    let ppid = stat.ppid;
+                    process.cmdline().and_then(|c| {
+                        let cmdline = c
+                            .into_iter()
+                            .fold("".to_owned(), |acc, val| acc + " " + &val); // TODO: why is this a Vec?
+                        Ok((pid, ppid, cmdline, process))
+                    })
+                })
+            }); // This should probably be illegal
 
-        let (pid, ppid, cmdline, process) = match filter_errors(combined_proc_info_result, fail_on_noperm) {
-            Some(Ok(tuple)) => tuple,
-            Some(Err(e)) => return Some(Err(e)),
-            None => return None,
-        };
+            let (pid, ppid, cmdline, process) =
+                match filter_errors(combined_proc_info_result, fail_on_noperm) {
+                    Some(Ok(tuple)) => tuple,
+                    Some(Err(e)) => return Some(Err(e)),
+                    None => return None,
+                };
 
-        Some(ProcResult::Ok(ProcNode { 
-            pid,
-            ppid, 
-            cmdline,
-            process,
-            children: vec![] 
-        }))
-    }).collect::<ProcResult<Vec<ProcNode>>>()?;
+            Some(ProcResult::Ok(ProcNode {
+                pid,
+                ppid,
+                cmdline,
+                process,
+                children: vec![],
+            }))
+        })
+        .collect::<ProcResult<Vec<ProcNode>>>()?;
     let Some(regex) = regex else {
         return Ok(proc_tree);
     };
-    let kv_pairs = (&proc_tree).into_iter().enumerate().map(|(i, proc_node)| (proc_node.pid, i));
+    let kv_pairs = (&proc_tree)
+        .into_iter()
+        .enumerate()
+        .map(|(i, proc_node)| (proc_node.pid, i));
     let proc_map: HashMap<_, _, RandomState> = HashMap::from_iter(kv_pairs);
     for idx in 0..proc_tree.len() {
         let proc_node = &proc_tree[idx];
         if proc_node.ppid != 0 {
-            let parent_idx = proc_map.get(&proc_node.ppid)
+            let parent_idx = proc_map
+                .get(&proc_node.ppid)
                 .expect(&format!("pid {} not found in proc_map", proc_node.ppid));
             proc_tree[*parent_idx].children.push(idx);
         }
     }
 
-    fn add_process_recursive(matched: &mut HashSet<usize>, proc_tree: &Vec<ProcNode>, proc_map: &HashMap<i32, usize>, proc_idx: usize) {
+    fn add_process_recursive(
+        matched: &mut HashSet<usize>,
+        proc_tree: &Vec<ProcNode>,
+        proc_map: &HashMap<i32, usize>,
+        proc_idx: usize,
+    ) {
         matched.insert(proc_idx);
         let proc_node = &proc_tree[proc_idx];
         for child_idx in &proc_node.children {
@@ -169,7 +211,8 @@ fn get_processes(regex: &Option<regex::Regex>, match_children: bool, fail_on_nop
         }
     }
     // Iterate through proc_tree; drop Processes that aren't matched, return ones that are.
-    let result = proc_tree.into_iter()
+    let result = proc_tree
+        .into_iter()
         .enumerate()
         .filter_map(|(process_idx, process_node)| {
             if matched.contains(&process_idx) {
@@ -266,14 +309,19 @@ fn get_smaps(processes: Vec<ProcNode>, fail_on_noperm: bool) -> ProcResult<Vec<P
 fn print_processes(processes: &Vec<ProcListing>) {
     println!("PID\tSTACK_PSS\tHEAP_PSS\tBIN_TEXT_PSS\tLIB_TEXT_PSS\tBIN_DATA_PSS\tLIB_DATA_PSS\tANON_MAP_PSS\tVDSO_PSS\tCMD");
     for proc_listing in processes {
-        let ProcListing { pid, cmdline, memory_ext, .. } = proc_listing;
-        let MemoryExt { 
-            stack_pss: stack, 
-            heap_pss: heap, 
-            bin_text_pss: bin_text, 
-            lib_text_pss: lib_text, 
-            bin_data_pss: bin_data, 
-            lib_data_pss: lib_data, 
+        let ProcListing {
+            pid,
+            cmdline,
+            memory_ext,
+            ..
+        } = proc_listing;
+        let MemoryExt {
+            stack_pss: stack,
+            heap_pss: heap,
+            bin_text_pss: bin_text,
+            lib_text_pss: lib_text,
+            bin_data_pss: bin_data,
+            lib_data_pss: lib_data,
             anon_map_pss: anon_map,
             vdso_pss: vdso,
         } = memory_ext;
