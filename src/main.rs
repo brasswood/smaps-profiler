983f2b81ccb1347f6dfb349b91b81ad5cd5285c6
Andrew Riachi <ariachi@ku.edu>
2025-05-02 15:02:17 -0500

    only graph categories which consume memory

--- src/main.rs
+++ src/main.rs
@@ -507,14 +507,20 @@ fn graph_memory(memory_series: Vec<MemoryExt>, out: PathBuf) {
     let first_series = vec![0.0; zero_series.len()];
     let mut prev_series = first_series;
     let mut draw_series = |series: &Vec<u64>, label: &str| {
+        let mut has_value = false;
         let series = prev_series
             .iter()
             .zip(series)
-            .map(|(a,b)| a + to_kib(*b))
+            .map(|(a,b)| {
+                if *b != 0 {has_value = true;}
+                a + to_kib(*b)
+            })
             .collect();
-        axes.fill_between(&xs, &prev_series, &series, &[Caption(label), FillAlpha(0.7)]);
         println!("{:?}", series);
-        prev_series = series;
+        if has_value {
+            axes.fill_between(&xs, &prev_series, &series, &[Caption(label), FillAlpha(0.7)]);
+            prev_series = series;
+        }
     };
 
     draw_series(&stack_series, "Stack");
